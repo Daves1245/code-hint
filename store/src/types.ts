6dd272129c33a03280aa4dda1d9f4317fd6f8216
David Santamaria <dsantamaria1245@gmail.com>
2026-07-13 05:41:14 -0400

    store: properly type screenDimensions

--- store/src/types.ts
+++ store/src/types.ts
@@ -2,7 +2,6 @@ export interface AppState {
   authState: AuthState;
   chatState: ChatState;
   uiState: UIState;
-  setUIState: (state: UIState) => void;
   setAuthState: (state: AuthState) => void;
   setChatState: (state: ChatState) => void;
 }
@@ -21,5 +20,9 @@ export interface Settings {}
 
 export interface UIState {
   input: string;
-  screenDimensions: [number, number];
+  state: { type: "error"; errmsg: string } | { type: "ok"; data: {} };
+  screenDimensions: {
+    height: number;
+    width: number;
+  }
 }
